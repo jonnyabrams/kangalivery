efb32c7e1f20e9adf188ec6b0ddea8faf015fe64
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-01 15:28:18 +0100

    feat: add more details to RestaurantScreen

--- src/screens/RestaurantScreen.js
+++ src/screens/RestaurantScreen.js
@@ -2,7 +2,13 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import { useLayoutEffect } from "react";
 import { View, Text, ScrollView, Image } from "react-native";
 import { TouchableOpacity } from "react-native";
-import { ArrowLeftIcon } from "react-native-heroicons/solid";
+import {
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  MapPinIcon,
+  StarIcon,
+  QuestionMarkCircleIcon,
+} from "react-native-heroicons/solid";
 
 import { urlFor } from "../../sanity";
 
@@ -44,6 +50,34 @@ const RestaurantScreen = () => {
           className="absolute p-2 bg-gray-100 rounded-full top-14 left-5"
         >
           <ArrowLeftIcon size={20} color="#00CCBB" />
+        </TouchableOpacity>
+      </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{title}</Text>
+          <View className="flex-row my-1 space-x-2">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> · {genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color="gray" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">Nearby · {address}</Text>
+            </View>
+          </View>
+          <Text className="pb-4 mt-2 text-gray-500">{short_description}</Text>
+        </View>
+
+        <TouchableOpacity className="flex-row items-center p-4 space-x-2 border-gray-300 border-y">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="flex-1 pl-2 font-bold text-md">
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB" />
         </TouchableOpacity>
       </View>
     </ScrollView>
