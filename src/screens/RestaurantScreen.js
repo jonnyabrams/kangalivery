90a6cd23787324e8438b742128b7857be66c1321
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-01 07:43:26 +0100

    feat: destructure out all params passed from RestaurantCard to RestaurantScreen

--- src/screens/RestaurantScreen.js
+++ src/screens/RestaurantScreen.js
@@ -1,6 +1,22 @@
+import { useRoute } from "@react-navigation/native";
 import { View, Text } from "react-native";
 
 const RestaurantScreen = () => {
+  const {
+    params: {
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      short_description,
+      dishes,
+      long,
+      lat,
+    },
+  } = useRoute();
+
   return (
     <View>
       <Text>RestaurantScreen</Text>
