9ba17defca33c5d6d3392b20d94e4f147bc4f4e4
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-02 09:37:30 +0100

    feat: minus/plus icons appear under dish on press

--- src/components/DishRow.js
+++ src/components/DishRow.js
@@ -1,28 +1,54 @@
+import { useState } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
+import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
+
 import { urlFor } from "../../sanity";
 
 const DishRow = ({ id, name, description, price, image }) => {
-  return (
-    <TouchableOpacity className="p-4 bg-white border border-gray-200">
-      <View className="flex-row">
-        <View className="flex-1 pr-2">
-          <Text className="mb-1 text-lg">{name}</Text>
-          <Text className="text-gray-400">{description}</Text>
-          <Text className="mt-2 text-gray-400">£{price}</Text>
-        </View>
+  const [isPressed, setIsPressed] = useState(false);
 
-        <View>
-          <Image
-            style={{
-              borderWidth: 1,
-              borderColor: "#F3F3F4",
-            }}
-            source={{ uri: urlFor(image).url() }}
-            className="w-20 h-20 p-4 bg-gray-300"
-          />
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        className={`p-4 bg-white border border-gray-200 ${isPressed && "border-b-0"}`}
+      >
+        <View className="flex-row">
+          <View className="flex-1 pr-2">
+            <Text className="mb-1 text-lg">{name}</Text>
+            <Text className="text-gray-400">{description}</Text>
+            <Text className="mt-2 text-gray-400">£{price}</Text>
+          </View>
+
+          <View>
+            <Image
+              style={{
+                borderWidth: 1,
+                borderColor: "#F3F3F4",
+              }}
+              source={{ uri: urlFor(image).url() }}
+              className="w-20 h-20 p-4 bg-gray-300"
+            />
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+
+      {isPressed && (
+        <View className="px-4 bg-white">
+          <View className="flex-row items-center pb-3 space-x-2">
+            <TouchableOpacity>
+              <MinusCircleIcon color="#00CCBB" size={40} />
+            </TouchableOpacity>
+            
+            <Text>0</Text>
+
+            <TouchableOpacity>
+              <PlusCircleIcon color="#00CCBB" size={40} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </>
   );
 };
 
