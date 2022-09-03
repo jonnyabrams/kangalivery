1161823352089a3c190e54e86a4ef64b8ae049ec
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-03 10:03:03 +0100

    feat: bring current restaurant & basket items into BasketScreen

--- src/screens/BasketScreen.js
+++ src/screens/BasketScreen.js
@@ -1,9 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+
+import { selectBasketItems } from "../features/basketSlice";
+import { selectRestaurant } from "../features/restaurantSlice";
 
 const BasketScreen = () => {
   const navigation = useNavigation();
-  
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const dispatch = useDispatch();
+
   return (
     <View>
       <Text>BasketScreen</Text>
