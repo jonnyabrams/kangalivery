d442fb3d7c5ebf45756edf20d9c9031f61b51bff
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-03 10:22:55 +0100

    feat: group same items together in basket (eg. burger x 5, not burger 5 times)

--- src/screens/BasketScreen.js
+++ src/screens/BasketScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -13,9 +13,20 @@ const BasketScreen = () => {
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
   const dispatch = useDispatch();
 
-  useMemo(() => {
-    
+  // group same items together so you don't get eg. the same burger 5 times in your basket -
+  // instead you get burger x 5 just once
+  // this is saying it the key exists, then push the item into that key
+  // so you end up with an object that's like { burger: [...]} which you can .length
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
+
+    setGroupedItemsInBasket(groupedItems);
   }, [items]);
+
+  console.log(groupedItemsInBasket)
 
   return (
     <View>
